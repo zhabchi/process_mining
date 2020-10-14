85762da6c4c03eb6c54709fd842e86d8d880afac
Ziad Habchi <Ziad.Habchi@gmail.com>
2020-10-14 17:34:07 +0400

    added comments

--- uapi_process_mining/app.R
+++ uapi_process_mining/app.R
@@ -19,16 +19,17 @@ library(ggplot2)
 library(edeaR)
 library(shinymanager)
 
-r <- GET("http://172.31.50.15:8094/api/Agencies/Get?ordered=1")
+r <-
+  GET("http://172.31.50.15:8094/api/Agencies/Get?ordered=1") #internal API to retrieve all agencies list in alphabetical order
 Agencies <-  fromJSON(fromJSON(content(r, "text")))
 #AllActivities <- c('OptimizedLowFareSearch','BookingStart','BookingAirSegment','BookingTraveler','BookingPricing','BookingPnrElement','BookingDisplay','BookingTerminal','BookingEnd','BookingAirPnrElement','AirTicketing','UniversalRecordRetrieve','AirRetrieveDocument')
 
-isDebug <- TRUE
+isDebug <-
+  TRUE # when set to true, the dataframe will load from a hardcoded file on the server, otherwise will load from Hive API.
 
-set_labels(
+set_labels(#set the label for login screen in shinymanager lib
   language = "en",
-  "Please authenticate" = "Please log in"
-)
+  "Please authenticate" = "Please log in")
 
 ui <- dashboardPage(
   skin = "blue",
@@ -39,6 +40,8 @@ ui <- dashboardPage(
   
   dashboardSidebar(
     width = 350,
+    
+    #Dropdown for Agencies, data loaded from API
     fluidRow(column(12, div(
       selectInput(
         "Agency_ID",
@@ -51,6 +54,7 @@ ui <- dashboardPage(
     
     fluidRow(column(12, div(style = "height:10px"))),
     
+    #Filter PCC dropdown
     fluidRow(column(12, div(
       selectInput(
         "PCC",
@@ -63,7 +67,7 @@ ui <- dashboardPage(
     
     fluidRow(column(12, div(style = "height:10px"))),
     
-    
+    #Checkbox to Exclude LFS requests
     fluidRow(column(12, div(
       checkboxInput(
         "includeLFS",
@@ -75,7 +79,7 @@ ui <- dashboardPage(
     
     fluidRow(column(12, div(style = "height:10px"))),
     
-    
+    #Data Selection
     fluidRow(column(12, div(
       dateInput(
         inputId = "Date",
@@ -84,6 +88,7 @@ ui <- dashboardPage(
       )
     ))),
     
+    #Time Selection
     fluidRow(column(12, div(
       column(
         6,
@@ -178,6 +183,7 @@ ui <- dashboardPage(
     ))))
   ),
   
+  #Dashboard will have the tabs of the output
   dashboardBody(tags$head(tags$style(
     HTML(
       '.main-header .logo {
@@ -198,7 +204,7 @@ ui <- dashboardPage(
     tabsetPanel(
       type = "tabs",
       
-      
+      #workflow animation
       tabPanel(
         title = "Workflow Visualization",
         icon = icon("project-diagram", class = "fas fa-project-diagram"),
@@ -247,16 +253,13 @@ ui <- dashboardPage(
   )))
 )
 
+#secure app will enable login screen
 ui <- secure_app(ui, enable_admin = TRUE)
 
 server <- function(input, output, session) {
-
-
-  res_auth <- secure_server(
-    check_credentials = check_credentials("../database/users.sqlite",
-                    passphrase = "Travelport"
-        )
-  )
+  #check if user is authorized against the user.sqlite database
+  res_auth <- secure_server(check_credentials = check_credentials("../database/users.sqlite",
+                                                                  passphrase = "Travelport"))
   
   hivedata <- reactive({
     fromDate <-
@@ -267,12 +270,14 @@ server <- function(input, output, session) {
     
     url <-
       "http://cvcpluapiss0059.tvlport.net:9000/queryWarehouse"
+    
+    #constructing the parameters depending on user's input
     param1 <- "{\"agency\":\""
     param2 <-  "\", \"txType\":\"\", \"startDate\":\""
     param3 <-   " 00:00\", \"endDate\":\""
     param4 <-   " 00:00\", \"successVal\":\"All\",
                         \"fieldsList\": \"log_id,log_ts,request_type_desc,success_ind,agency_name,pseudo_city_code,traceid,session_key,\",
-                        \"rowLimit\":\"10000\",
+                        \"rowLimit\":\"30000\",
                         \"outputFormat\":\"json\",
                         \"orderBy\":\"\",
                         \"ascendDescend\":\"ASC\",
@@ -309,9 +314,14 @@ server <- function(input, output, session) {
       param5 <- ""
     
     param6 <-
-      paste("\"userAuth\":\"", res_auth$user , "\", \"email\": \"api_user@donotreply.travelport.com\",\"password\":\"4293ea3b8517c015c542643439b26fe46a694baf9f2598dbd196fd98aac0cdaa\"}" , sep ="")
+      paste(
+        "\"userAuth\":\"",
+        res_auth$user ,
+        "\", \"email\": \"api_user@donotreply.travelport.com\",\"password\":\"4293ea3b8517c015c542643439b26fe46a694baf9f2598dbd196fd98aac0cdaa\"}" ,
+        sep = ""
+      )
     
-   
+    
     jsonargs <-
       paste(
         param1,
@@ -325,7 +335,7 @@ server <- function(input, output, session) {
         param6,
         sep = ""
       )
-    print(jsonargs)
+    #print(jsonargs)
     parambody <- list(json = jsonargs)
     ##
     msgId <-
@@ -333,7 +343,8 @@ server <- function(input, output, session) {
                        type = "message" ,
                        duration = NULL)
     
-    if(isDebug)
+    #section will load data from file
+    if (isDebug)
     {
       removeNotification(msgId)
       hivedata <- read_csv("Workflow from Website.csv")
@@ -350,11 +361,12 @@ server <- function(input, output, session) {
       
       hivedata
     }
-    else if(!isDebug)
+    else if (!isDebug)
+      #call hive API for data
     {
       res = POST(url, body =  parambody , encode = "form")
-
-
+      
+      
       ##check response code
       if (res$status_code == 200)
       {
@@ -368,11 +380,14 @@ server <- function(input, output, session) {
           hivedata$log_ts <-
             as.POSIXct(hivedata$log_ts, format = "%Y-%m-%dT%H:%M:%OS", tz = 'UTC')
           
-          #filtering top 100000 records for performance reasons
+          #filtering top x records for performance reasons
           hivedata <- head(hivedata, 30000)
+          
+          #filtering out excluded trace ids
           hivedata <- hivedata %>%
             filter(!(traceid  %in% input$ExclTraceIDs))
           
+          #filtering out exculded PCCs
           if (input$PCC != "All")
           {
             hivedata <- hivedata  %>%  filter(pseudo_city_code == input$PCC)
@@ -381,6 +396,7 @@ server <- function(input, output, session) {
           hivedata
         }
         else
+          #case where hive do not return data
         {
           removeNotification(msgId)
           msgId <-
@@ -389,6 +405,7 @@ server <- function(input, output, session) {
         }
       }
       else
+        #case when connection to server fails.
       {
         removeNotification(msgId)
         msgId <-
@@ -398,10 +415,11 @@ server <- function(input, output, session) {
     }
   })
   
+  #generate even log using eventlog function
   eventloghive <- reactive({
     data <- hivedata()
-
-    if(!is.null(data)){
+    
+    if (!is.null(data)) {
       data %>% #a data.frame with the information in the table above
         filter(!(traceid %in% c("", " "))) %>%
         filter(!(is.na(traceid))) %>%
@@ -419,7 +437,7 @@ server <- function(input, output, session) {
         ) %>%
         filter_activity_frequency(percentage = input$frequency)
     }
-    else 
+    else
       NULL
   })
   
@@ -433,169 +451,165 @@ server <- function(input, output, session) {
     }
     else
     {
-        output$traceID_aggr <- DT::renderDataTable({
-          data <- hivedata()
-          if(!is.null(data))
-          {
-            hivedataaggr <-  data %>%
+      #display the usage of traceid
+      output$traceID_aggr <- DT::renderDataTable({
+        data <- hivedata()
+        if (!is.null(data))
+        {
+          hivedataaggr <-  data %>%
             group_by(traceid) %>%
             summarise(CountTrace = n()) %>%
             arrange(desc(CountTrace))
-             hivedataaggr
-          }
-          else
-            NULL
-        })
-        
-        
-        output$traceId_plot <-  renderPlot({
-          data <- hivedata()
-          if(!is.null(data))
-          {
-            totalReq <- nrow(hivedata())
-            hivedataplot <-
-              hivedata() %>% group_by(request_type_desc) %>%
-              mutate(emptyTrace = ifelse(is.na(traceid), 1 , 0)) %>%
-              summarise(
-                count_req =  n() ,
-                emptytracecount = sum(emptyTrace) ,
-                percUsage = (n() - sum(emptyTrace)) / n()
-              )
-            
-            ggplot(hivedataplot, aes(
-              x = reorder(`request_type_desc` , percUsage * 100),
-              y = percUsage * 100
-            )) +
-              geom_bar(stat = 'identity') +
-              xlab('Request') +
-              ylab('Number of Requests') +
-              coord_flip()
-          }
-        })
-        
-        
-        output$RawData <- DT::renderDataTable({
-          hivedata()
-        })
-        
-        
-        tempSlcted <- input$ExclTraceIDs
-        traceIds <- unique(hivedata()$traceid)
-        traceIds <- traceIds[!is.na(traceIds)] #remove NA
-        
-        updateSelectInput(session,
-                          "ExclTraceIDs",
-                          choices = traceIds,
-                          selected = tempSlcted)
-        
-        
-        tempSlcted <- input$PCC
-        Pccs <- unique(hivedata()$pseudo_city_code)
-        Pccs <- prepend(Pccs , "All")
-        updateSelectInput(session,
-                          "PCC",
-                          choices = Pccs,
-                          selected = tempSlcted)
-        
-        
-        
-        
-        output$Pr_map <- renderGrViz({
-         
+          hivedataaggr
+        }
+        else
+          NULL
+      })
+      
+      
+      output$traceId_plot <-  renderPlot({
+        data <- hivedata()
+        if (!is.null(data))
+        {
+          totalReq <- nrow(hivedata())
+          hivedataplot <-
+            hivedata() %>% group_by(request_type_desc) %>%
+            mutate(emptyTrace = ifelse(is.na(traceid), 1 , 0)) %>%
+            summarise(
+              count_req =  n() ,
+              emptytracecount = sum(emptyTrace) ,
+              percUsage = (n() - sum(emptyTrace)) / n()
+            )
           
-          if (nrow(hivedata) == 0) {
-            showNotification("No data in Hive for the selected parameters." ,
-                             type = "warning")
-            pp <- NULL
-          }
-          else
-          {
-            pp <-
-              eventloghive() %>%
-              process_map(
-                type = frequency("absolute"),
-                sec_edges = performance(mean, "mins"),
-                rankdir = "TB"
-              )
-          }
-        })
-    }
-    
-    output$process <- renderProcessanimater(expr = {
+          ggplot(hivedataplot, aes(
+            x = reorder(`request_type_desc` , percUsage * 100),
+            y = percUsage * 100
+          )) +
+            geom_bar(stat = 'identity') +
+            xlab('Request') +
+            ylab('Number of Requests') +
+            coord_flip()
+        }
+      })
       
-      loghiv <- eventloghive()
-      if(!is.null(loghiv))
-      {
-        graph <-
-          process_map(
+      #display raw data in a table
+      output$RawData <- DT::renderDataTable({
+        hivedata()
+      })
+      
+      #update the dropdown list of trace ID
+      tempSlcted <- input$ExclTraceIDs
+      traceIds <- unique(hivedata()$traceid)
+      traceIds <- traceIds[!is.na(traceIds)] #remove NA
+      updateSelectInput(session,
+                        "ExclTraceIDs",
+                        choices = traceIds,
+                        selected = tempSlcted)
+      
+      #update the dropdown list of PCC
+      tempSlcted <- input$PCC
+      Pccs <- unique(hivedata()$pseudo_city_code)
+      Pccs <- prepend(Pccs , "All")
+      updateSelectInput(session,
+                        "PCC",
+                        choices = Pccs,
+                        selected = tempSlcted)
+      
+      
+      
+      
+      #output$Pr_map <- renderGrViz({
+      
+      
+      #if (nrow(hivedata) == 0) {
+      #  showNotification("No data in Hive for the selected parameters." ,
+      #                   type = "warning")
+      #  pp <- NULL
+      #}
+      #else
+      #{
+      #  pp <-
+      #    eventloghive() %>%
+      #    process_map(
+      #      type = frequency("absolute"),
+      #      sec_edges = performance(mean, "mins"),
+      #      rankdir = "TB"
+      #    )
+      #}
+      #})
+      
+      output$process <- renderProcessanimater(expr = {
+        loghiv <- eventloghive()
+        if (!is.null(loghiv))
+        {
+          graph <-
+            process_map(
+              loghiv,
+              render = F,
+              type = frequency("absolute"),
+              sec_edges = performance(mean, "mins"),
+              rankdir = "TB"
+            )
+          
+          model <- DiagrammeR::add_global_graph_attrs(
+            graph,
+            attr = "rankdir",
+            value = "TB",
+            attr_type = "graph"
+          )
+          
+          animate_process(
             loghiv,
-            render = F,
-            type = frequency("absolute"),
-            sec_edges = performance(mean, "mins"),
-            rankdir = "TB"
+            model,
+            mode = "relative",
+            mapping = token_aes(color = token_scale("red")),
+            duration = 20,
+            initial_state = "paused"
           )
-        
-        model <- DiagrammeR::add_global_graph_attrs(graph,
-                                                    attr = "rankdir",
-                                                    value = "TB",
-                                                    attr_type = "graph")
-        
-        animate_process(
-          loghiv,
-          model,
-          mode = "relative",
-          mapping = token_aes(color = token_scale("red")),
-          duration = 20,
-          initial_state = "paused"
+        }
+        else
+          NULL
+      })
+      
+      output$downloadRawData <- downloadHandler(
+        filename = function() {
+          paste(input$Agency_ID , Sys.Date(), "data.csv", sep = "")
+        },
+        content = function(file) {
+          write.csv(hivedata() , file)
+        }
+      )
+      
+      output$loopBox <- renderValueBox({
+        SL <- number_of_selfloops(eventloghive())
+        print(SL)
+        valueBox(
+          value = SL,
+          "Approval",
+          icon = icon("thumbs-up", lib = "glyphicon")
         )
-      }
-      else 
-        NULL
+      })
       
-      
-    })
-    
-    output$downloadRawData <- downloadHandler(
-      filename = function() {
-        paste(input$Agency_ID , Sys.Date(), "data.csv", sep = "")
-      },
-      content = function(file) {
-        write.csv(hivedata() , file)
-      }
-    )
-    
-    output$loopBox <- renderValueBox({
-      
-      SL <- number_of_selfloops(eventloghive())
-      print(SL)
-      
-      valueBox(value = SL, "Approval", icon = icon("thumbs-up", lib = "glyphicon"))
-               
-               
-    })    
-    
-    
-    output$downloadProcessMap <- downloadHandler(
-      filename = function() {
-        paste(input$Agency_ID , Sys.Date(), ".svg", sep = "")
-      },
-      content = function(file) {
-        graphExport <-
-          eventloghive() %>%
-          process_map(
-            type = frequency("absolute"),
-            sec_edges = performance(mean, "mins"),
-            rankdir = "TB",
-            render = FALSE
-          )
-        
-        export_graph(graphExport,
-                     file_name = file ,
-                     file_type = "SVG")
-        
-      }
-    )
-    
+      output$downloadProcessMap <- downloadHandler(
+        filename = function() {
+          paste(input$Agency_ID , Sys.Date(), ".svg", sep = "")
+        },
+        content = function(file) {
+          graphExport <-
+            eventloghive() %>%
+            process_map(
+              type = frequency("absolute"),
+              sec_edges = performance(mean, "mins"),
+              rankdir = "TB",
+              
+              render = FALSE
+            )
+          export_graph(graphExport,
+                       file_name = file ,
+                       file_type = "SVG")
+        }
+      )
+    }
   })
   
 }
